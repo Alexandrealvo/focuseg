50bd3e57bd3351ace027c02c5ee96082a71c97a8
Alexandrealvo <alexandrebacana@gmail.com>
2021-03-08 20:07:40 -0300

    Erro no local_auth

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Alexandre Rocha on 19/02/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
