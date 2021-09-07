a09e9f8309fee0bdf3d92939f514385233ba95ea
alexandref13 <alexfernandes199922@gmail.com>
2021-09-07 16:11:43 -0300

    a

--- lib/src/pages/mapa_agenda.dart
+++ lib/src/pages/mapa_agenda.dart
@@ -12,7 +12,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:http/http.dart' as http;
-import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:focus/src/pages/info_servicos.dart';
 
@@ -150,6 +149,7 @@ class MapaAgendaState extends State<MapaAgenda> {
                 builder: (context) =>
                     HomePage(id, nome, tipo, imgperfil, email)),
             (Route<dynamic> route) => false);
+        return false;
       },
       child: Scaffold(
         appBar: AppBar(
@@ -753,144 +753,6 @@ class MapaAgendaState extends State<MapaAgenda> {
 
                   final Uint8List markerIconCliente =
                       await getBytesFromAsset('images/iconUserMap.png', 90);
-
-                  //final imagePath = "images/logo.png";
-
-                  /* final imageURL =
-                      'https://www.focuseg.com.br/areadm/downloads/fotosprofissionais/${mapaAgenda[index].imgperfil}';
-
-                  final File markerImageFile =
-                      await DefaultCacheManager().getSingleFile(imageURL);
-                  final Uint8List markerImageBytes =
-                      await markerImageFile.readAsBytes();
-
-                  ui.Codec codec = await ui
-                      .instantiateImageCodec(markerImageBytes, targetWidth: 80);
-                  ui.FrameInfo fi = await codec.getNextFrame();
-
-                  final Uint8List markerImage = (await fi.image
-                          .toByteData(format: ui.ImageByteFormat.png))
-                      .buffer
-                      .asUint8List();
-
-                  Future<ui.Image> getImageFromPath(String imagePath) async {
-                    File imageFile = File(imagePath);
-
-                    Uint8List imageBytes = imageFile.readAsBytesSync();
-
-                    final Completer<ui.Image> completer = new Completer();
-
-                    ui.decodeImageFromList(imageBytes, (ui.Image img) {
-                      return completer.complete(img);
-                    });
-
-                    return completer.future;
-                  }
-
-                  Future<BitmapDescriptor> getMarkerIcon(
-                      String imagePath, Size size) async {
-                    final ui.PictureRecorder pictureRecorder =
-                        ui.PictureRecorder();
-                    final Canvas canvas = Canvas(pictureRecorder);
-
-                    final Radius radius = Radius.circular(size.width / 2);
-
-                    final Paint tagPaint = Paint()..color = Colors.blue;
-                    final double tagWidth = 40.0;
-
-                    final Paint shadowPaint = Paint()
-                      ..color = Colors.blue.withAlpha(100);
-                    final double shadowWidth = 15.0;
-
-                    final Paint borderPaint = Paint()..color = Colors.white;
-                    final double borderWidth = 3.0;
-
-                    final double imageOffset = shadowWidth + borderWidth;
-
-                    // Add shadow circle
-                    canvas.drawRRect(
-                        RRect.fromRectAndCorners(
-                          Rect.fromLTWH(0.0, 0.0, size.width, size.height),
-                          topLeft: radius,
-                          topRight: radius,
-                          bottomLeft: radius,
-                          bottomRight: radius,
-                        ),
-                        shadowPaint);
-
-                    // Add border circle
-                    canvas.drawRRect(
-                        RRect.fromRectAndCorners(
-                          Rect.fromLTWH(
-                              shadowWidth,
-                              shadowWidth,
-                              size.width - (shadowWidth * 2),
-                              size.height - (shadowWidth * 2)),
-                          topLeft: radius,
-                          topRight: radius,
-                          bottomLeft: radius,
-                          bottomRight: radius,
-                        ),
-                        borderPaint);
-
-                    // Add tag circle
-                    canvas.drawRRect(
-                        RRect.fromRectAndCorners(
-                          Rect.fromLTWH(
-                              size.width - tagWidth, 0.0, tagWidth, tagWidth),
-                          topLeft: radius,
-                          topRight: radius,
-                          bottomLeft: radius,
-                          bottomRight: radius,
-                        ),
-                        tagPaint);
-
-                    // Add tag text
-                    TextPainter textPainter =
-                        TextPainter(textDirection: TextDirection.ltr);
-                    textPainter.text = TextSpan(
-                      text: '1',
-                      style: TextStyle(fontSize: 20.0, color: Colors.white),
-                    );
-
-                    textPainter.layout();
-                    textPainter.paint(
-                        canvas,
-                        Offset(
-                            size.width - tagWidth / 2 - textPainter.width / 2,
-                            tagWidth / 2 - textPainter.height / 2));
-
-                    // Oval for the image
-                    Rect oval = Rect.fromLTWH(
-                        imageOffset,
-                        imageOffset,
-                        size.width - (imageOffset * 2),
-                        size.height - (imageOffset * 2));
-
-                    // Add path for oval image
-                    canvas.clipPath(Path()..addOval(oval));
-
-                    // Add image
-                    ui.Image image = await getImageFromPath(
-                        imagePath); // Alternatively use your own method to get the image
-                    paintImage(
-                        canvas: canvas,
-                        image: image,
-                        rect: oval,
-                        fit: BoxFit.fitWidth);
-
-                    // Convert canvas to image
-                    final ui.Image markerAsImage = await pictureRecorder
-                        .endRecording()
-                        .toImage(size.width.toInt(), size.height.toInt());
-
-                    // Convert image to bytes
-                    final ByteData byteData = await markerAsImage.toByteData(
-                        format: ui.ImageByteFormat.png);
-                    final Uint8List uint8List = byteData.buffer.asUint8List();
-
-                    return BitmapDescriptor.fromBytes(uint8List);
-                  }*/
 
                   if (mapaAgenda[index].ctlcheckin == "1") {
                     setState(() {
